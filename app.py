718b41c057c01b5122a237b6b94675dffce9db1a
Zhirrr <zahirdanfakhri@gmail.com>
2021-03-14 21:51:01 +0700

    first commit

--- app.py
+++ app.py
@@ -147,6 +147,60 @@ def tigadimensi():
          }
         return js
 
+@app.route('/api/text3d-2', methods=['GET'])
+def tigadimensi2():
+    from lib.text3dke2 import tulis
+    text = request.args.get('text')
+    tulis=tulis(text)
+    for i in tulis.tulis():
+        i.save('gambar.jpg')
+        image = open('gambar.jpg', 'rb')
+        image_read = image.read()
+        image_64_encode = base64.encodebytes(image_read)
+        url = 'https://api.imgbb.com/1/upload'
+        par = {
+         'key':'761ea2d5575581057a799d14e9c78e28',
+         'image':image_64_encode,
+         'name':'support zahirr',
+         'expiration': 60
+         }
+        headers = {
+         'Accept': 'application/json'
+         }
+        req = requests.post(url,data=par, headers=headers)
+        p = req.json()['data']['display_url']
+        js = {
+         "results":p
+         }
+        return js
+
+@app.route('/api/text3d-3', methods=['GET'])
+def tigadimensi3():
+    from lib.text3dke3 import tulis
+    text = request.args.get('text')
+    tulis=tulis(text)
+    for i in tulis.tulis():
+        i.save('gambar.jpg')
+        image = open('gambar.jpg', 'rb')
+        image_read = image.read()
+        image_64_encode = base64.encodebytes(image_read)
+        url = 'https://api.imgbb.com/1/upload'
+        par = {
+         'key':'761ea2d5575581057a799d14e9c78e28',
+         'image':image_64_encode,
+         'name':'support zahirr',
+         'expiration': 60
+         }
+        headers = {
+         'Accept': 'application/json'
+         }
+        req = requests.post(url,data=par, headers=headers)
+        p = req.json()['data']['display_url']
+        js = {
+         "results":p
+         }
+        return js
+
 
 if __name__ == '__main__':
     app.run()
